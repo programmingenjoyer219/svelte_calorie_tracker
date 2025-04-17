5749977af6b9b18b793833af4243234c2f5e8fd5
Yuganshu <yuganshu219@gmail.com>
2025-04-17 16:55:31 +0530

    config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{svelte,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    themes: [],
-  },
-}
+    content: ['./index.html', './src/**/*.{svelte,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [typography, daisyui],
+    daisyui: {
+        themes: [],
+    },
+};
